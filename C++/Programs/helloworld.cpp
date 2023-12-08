5afcb5d6d12c0c3d1cb54996ec53e27f8d921b64
Nitin Bhardwaj <118904343+nbcodeblog@users.noreply.github.com>
2023-12-08 18:30:59 +0530

    Add comments

--- C++/Programs/helloworld.cpp
+++ C++/Programs/helloworld.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main()
 {
-    cout<<"Hello World";
+    cout<<"Hello World"; // It is up to you if you want to include the keyword followed by the operator for some objects or not like - std::
     return 0;
 }
