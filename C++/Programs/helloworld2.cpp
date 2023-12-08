c2b879ac9bee4cb23bacda776c82e22108e0cf00
Nitin Bhardwaj <118904343+nbcodeblog@users.noreply.github.com>
2023-12-08 18:31:33 +0530

    Initial commit

---
+++ C++/Programs/helloworld2.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+int main()
+{
+    std::cout<<"Hello World"; // It is up to you if you want to include the standard namespace library or not like - using namespace std;
+    return 0;
+}
